3588141459ce123d0ea9a5ad8a05c02c49f9002d
Jens Kouros <jens.kouros@posteo.de>
2025-08-15 17:55:26 +0200

    Add datatype completion provider

--- src/types/completion.ts
+++ src/types/completion.ts
@@ -5,3 +5,4 @@ export interface Searchable {
 export interface Distribution extends Searchable {}
 export interface StanFunction extends Searchable {}
 export interface Keyword extends Searchable {}
+export interface Datatype extends Searchable {}
