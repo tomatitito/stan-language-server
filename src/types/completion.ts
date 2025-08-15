86c966865a32cf05a3913bfdf649ca51225176e6
Jens Kouros <jens.kouros@posteo.de>
2025-08-15 18:04:50 +0200

    Add constraint completion provider

--- src/types/completion.ts
+++ src/types/completion.ts
@@ -6,3 +6,4 @@ export interface Distribution extends Searchable {}
 export interface StanFunction extends Searchable {}
 export interface Keyword extends Searchable {}
 export interface Datatype extends Searchable {}
+export interface Constraint extends Searchable {}
