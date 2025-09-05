058a369fae71c629a7bff4667059495e55b15b50
Jens Kouros <jens.kouros@posteo.de>
2025-09-05 22:53:21 +0200

    Fix bug when reading includes from filesystem

--- src/handlers/compilation/includes.ts
+++ src/handlers/compilation/includes.ts
@@ -18,6 +18,10 @@ export async function handleIncludes(
 ): Promise<Record<Filename, FileContent>> {
   const includeFilenames = getFilenames(document.getText());
 
+  if (includeFilenames.length === 0) {
+    return {};
+  }
+
   const allResults = await Promise.all(
     includeFilenames.map(async (filename) => {
       try {
@@ -41,7 +45,7 @@ export async function handleIncludes(
     ([_, content]) => !isFilePathError(content),
   ) as [Filename, FileContent][];
 
-  return Promise.resolve(Object.fromEntries(validResults));
+  return Object.fromEntries(validResults);
 }
 
 const readIncludedFile = async (
@@ -83,15 +87,21 @@ const readIncludedFileFromWorkspace = (
   filename: Filename,
   currentDir: string,
 ): Promise<FileContent | FilePathError> => {
-  workspaceFolders = [
+  const searchFolders = [
     { uri: currentDir.toString(), name: "current directory" },
     ...workspaceFolders,
   ];
 
-  const includedFile = workspaceFolders
-    .map((folder) => folder.uri + "/" + filename)
-    .map((path) => documentManager.get(path))
-    .filter((doc): doc is TextDocument => doc !== undefined)[0];
+  const paths = searchFolders.map((folder) => folder.uri + "/" + filename);
+  const documents = paths.map((path) => {
+    const doc = documentManager.get(path);
+    return { path, doc };
+  });
+  
+  
+  const includedFile = documents
+    .filter(({ doc }) => doc !== undefined)
+    .map(({ doc }) => doc)[0];
 
   if (!includedFile) {
     return Promise.resolve({ msg: `File not found: ${filename}` });
@@ -104,12 +114,12 @@ const readIncludedFileFromFileSystem = async (
   filename: Filename,
   currentDir: string,
 ): Promise<FileContent | FilePathError> => {
-  const includePath = URL.parse(join(currentDir, filename));
+  const localPath = join(currentDir, filename);
 
-  if (includePath) {
-    return promises.readFile(includePath, "utf-8");
+  try {
+    return await promises.readFile(localPath, "utf-8");
+  } catch (error) {
+    return Promise.resolve({ msg: `File not found: ${filename}` });
   }
-
-  return Promise.resolve({ msg: `File not found: ${filename}` });
 };
 
