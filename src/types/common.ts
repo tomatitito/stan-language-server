644e1d856ed848fd18f7a01437a0918a17694a68
Brian Ward <bward@flatironinstitute.org>
2025-10-03 19:51:23 +0000

    Consolidate diagnostic logic, avoid duplicating types etc

--- src/types/common.ts
+++ src/types/common.ts
@@ -5,25 +5,4 @@ export interface Position {
   character: number;
 }
 
-type StancSuccess = {
-  errors: undefined;
-  result: string;
-  warnings?: string[];
-};
-
-type StancFailure = {
-  errors: string[];
-  result: undefined;
-  warnings?: string[];
-};
-
-export type StancReturn = StancSuccess | StancFailure;
-
-export type StancFunction = (
-  filename: string,
-  code: string,
-  options: string[],
-  includes?: Record<string, string>,
-) => StancReturn;
-
 export type FileSystemReader = (filename: Filename) => Promise<FileContent>;
