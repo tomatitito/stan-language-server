181991f9b5f98662eb0fc4f0e1b635a66ba1a5b9
Jens Kouros <jens.kouros@posteo.de>
2025-10-05 22:51:29 +0200

    Refactor provideDiagnostics and move back to provider module

--- src/handlers/diagnostics.ts
+++ src/handlers/diagnostics.ts
@@ -1,53 +1,17 @@
 import {
   Diagnostic,
-  DiagnosticSeverity,
-  TextDocuments,
+  DiagnosticSeverity, TextDocuments,
   WorkspaceFolder,
   type DocumentDiagnosticParams,
-  type RemoteConsole,
+  type RemoteConsole
 } from "vscode-languageserver";
 import { TextDocument } from "vscode-languageserver-textdocument";
 import { handleCompilation, type Settings } from "./compilation/compilation";
 import type { FileSystemReader } from "../types";
 import { SERVER_ID } from "../constants";
-import type { StancReturn } from "stanc3";
 import {
-  getErrorMessage,
-  getWarningMessage,
-  rangeFromMessage,
+  provideDiagnostics
 } from "../language/diagnostics/linter";
-
-export function provideDiagnostics(compilerResult: StancReturn): Diagnostic[] {
-  const diagnostics: Diagnostic[] = [];
-
-  if (compilerResult.errors) {
-    for (const error of compilerResult.errors) {
-      const range = rangeFromMessage(error);
-      if (range) {
-        diagnostics.push({
-          range,
-          severity: DiagnosticSeverity.Error,
-          message: getErrorMessage(error),
-          source: SERVER_ID,
-        });
-      }
-    }
-  }
-  if (compilerResult.warnings) {
-    for (const warning of compilerResult.warnings) {
-      const range = rangeFromMessage(warning);
-      if (range) {
-        diagnostics.push({
-          range,
-          severity: DiagnosticSeverity.Warning,
-          message: getWarningMessage(warning),
-          source: SERVER_ID,
-        });
-      }
-    }
-  }
-  return diagnostics;
-}
 
 export async function handleDiagnostics(
   params: DocumentDiagnosticParams,
@@ -70,7 +34,25 @@ export async function handleDiagnostics(
     logger,
     reader
   );
-  const stanDiagnostics = provideDiagnostics(compilerResult);
 
-  return stanDiagnostics;
+  const diagnostics: Diagnostic[] = provideDiagnostics(compilerResult).map((diagnostic) => {
+    if (diagnostic.severity === "error") {
+      return {
+        range: diagnostic.range,
+        severity: DiagnosticSeverity.Error,
+        message: diagnostic.message,
+        source: SERVER_ID
+      }
+    }
+    else if (diagnostic.severity === "warning") {
+      return {
+        range: diagnostic.range,
+        severity: DiagnosticSeverity.Warning,
+        message: diagnostic.message,
+        source: SERVER_ID
+      }
+    }
+  }).filter((diagnostic) => diagnostic !== undefined);
+
+  return diagnostics;
 }
