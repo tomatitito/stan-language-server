b37bacc4584b147c54d45633304ebfbb2f0c5363
Brian Ward <bward@flatironinstitute.org>
2026-03-27 15:36:19 +0000

    Remove now-defunct types

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,8 +1,3 @@
 import type { FileContent, Filename } from "../handlers/compilation/includes";
 
-export interface Position {
-  line: number;
-  character: number;
-}
-
 export type FileSystemReader = (filename: Filename) => Promise<FileContent>;
